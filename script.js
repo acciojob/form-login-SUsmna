c84a62ab9321f2fe2c542900c81a57390fecff33
SUsmna <159513831+SUsmna@users.noreply.github.com>
2024-06-05 12:09:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
 function getFormvalue() {
     //Write your code here
+	const firstName = document.getElementById('firstname'.value);
 
+	const lastName = document.getElementById('firstname'.value);
+
+	const lastNameParts = lastName.split('');
+	let middleName = '';
+	if(lastNameParts.length>1){
+		middleName = lastNameParts.slice(0,-1).join('');
+		lastName = lastNameParts[lastNameParts.length-1];
+	}
+	id(middleName !== ''){
+		alert(`${firstName} ${middleName} ${lastName}`);
+	}else{
+		alert(`${firstName} ${lastName}`);
+	}
 }
